9fb1796b656b4561dd1195f4566a6af8db52085e
Leonel Adame <jleoneldev@MacBook-Air-de-Leonel.local>
2019-05-28 18:52:06 -0500

    Testing framework installation

---
+++ src/test/unit/controllers/salaryController.test.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import { salaryController } from '../../../controllers/salaryController';
+
+describe('true or false', () => {
+  it('true is true', () => {
+    expect(true).to.eql(true);
+  });
+
+  it('false is false', () => {
+    expect(false).to.eql(false);
+  });
+});
